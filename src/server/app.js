ddb3dcf4d7a1fe3246b4419e25d36ac072bf9e04
Gang Li <gli@redhat.com>
2018-11-06 17:32:48 +0800

    Use current fake list for single repo rest call

--- src/server/app.js
+++ src/server/app.js
@@ -34,15 +34,46 @@ app.get('/api/admin/schedule/store/all/disable-timeout', function (req, res){
 } );
 
 app.get('/api/admin/stores/maven/remote/:name', function(req, res){
-  let name=req.params.name;
-  if(name){
-    try{
-      let result = require(`./mock/store/remote/${name}.json`);
-      res.status(200).json(result);
-    }catch(error){
-      res.status(500).json({error: error});
-    }
-  }else{
+  var name=req.params.name;
+  if(!name){
     res.status(400).json({error: "Missing store name"});
+  }else{
+    let remoteList  = require('./mock/list/FakeRemoteList.json');
+    let result = remoteList.items.find(item=>item.name===name);
+    if(result){
+      res.status(200).json(result);
+    }else{
+      res.status(404).json({error: "No such store!"});
+    }
+  }
+});
+
+app.get('/api/admin/stores/maven/hosted/:name', function(req, res){
+  var name=req.params.name;
+  if(!name){
+    res.status(400).json({error: "Missing store name"});
+  }else{
+    let remoteList  = require('./mock/list/FakeHostedList.json');
+    let result = remoteList.items.find(item=>item.name===name);
+    if(result){
+      res.status(200).json(result);
+    }else{
+      res.status(404).json({error: "No such store!"});
+    }
+  }
+});
+
+app.get('/api/admin/stores/maven/group/:name', function(req, res){
+  var name=req.params.name;
+  if(!name){
+    res.status(400).json({error: "Missing store name"});
+  }else{
+    let remoteList  = require('./mock/list/FakeGroupList.json');
+    let result = remoteList.items.find(item=>item.name===name);
+    if(result){
+      res.status(200).json(result);
+    }else{
+      res.status(404).json({error: "No such store!"});
+    }
   }
 });
