2b9c1299cf2d99ce5ad3475796e7034ff578bea5
Gang Li <gli@redhat.com>
2018-11-19 17:30:47 +0800

    Sync webpack prod & dev config

--- webpack.config-prod.js
+++ webpack.config-prod.js
@@ -34,6 +34,14 @@ module.exports = {
             loader: "less-loader"
           }
         ]
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader'
+          },
+        ]
       }
     ]
   }
