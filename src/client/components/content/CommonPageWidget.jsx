ab962a7c490dc8e4e2e18e3a5def283a68825b83
Gang Li <gli@redhat.com>
2019-01-09 20:54:13 +0800

    Add StoreNameSection in CommonPageWidget

--- src/client/components/content/CommonPageWidget.jsx
+++ src/client/components/content/CommonPageWidget.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {Utils} from '../CompUtils.js';
+import {APP_ROOT} from "../ComponentConstants.js";
 
 const LocalURLSection = ({storeKey}) => <div className="left-half">
-        <label>Local URL:</label>{' '}
-        <a href={Utils.storeHref(storeKey)} target="_new">{Utils.storeHref(storeKey)}</a>
-    </div>;
+    <label>Local URL:</label>{' '}
+    <a href={Utils.storeHref(storeKey)} target="_new">{Utils.storeHref(storeKey)}</a>
+  </div>;
 
 LocalURLSection.propTypes = {
-    storeKey: PropTypes.string
+  storeKey: PropTypes.string
 };
 
 const CapabilitiesSection = ({options}) => <div className="left-half">
     <label>Capabilities:</label>{' '}
     {
-        options.map(option => <div key={option.title} className="options">
-                <span className="key">{option.icon} </span>
-            </div>)
+      options.map(option => <div key={option.title} className="options">
+          <span className="key">{option.icon} </span>
+        </div>)
     }
-    </div>;
+  </div>;
 
 CapabilitiesSection.propTypes = {
-    options: PropTypes.array
+  options: PropTypes.array
 };
 
-export {LocalURLSection, CapabilitiesSection};
+const StoreNameSection = ({store, storeClass}) => <div className="fieldset-caption">
+    <Link to={`${APP_ROOT}/${store.type}/${store.packageType}/view/${store.name}`}>
+      <span className={storeClass}>{store.packageType}-{store.name}</span>
+    </Link>
+  </div>;
+
+StoreNameSection.propTypes = {
+  store: PropTypes.object,
+  storeClass: PropTypes.string
+};
+
+
+export {LocalURLSection, CapabilitiesSection, StoreNameSection};
